7f7d258e543f16da63da9e6e5b569ce9565c1724
Barreto <v.barreto.and@gmail.com>
2021-06-01 23:40:44 -0300

    feat: add empty-state of dashboard

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,11 +1,33 @@
+import { createStyles, Grid, makeStyles, Theme, Typography } from '@material-ui/core';
+import { BarChart } from '@material-ui/icons';
 import Head from 'next/head';
-import { SyntheticEvent, useContext, useEffect, useState } from 'react';
+import React, { SyntheticEvent, useContext, useEffect, useState } from 'react';
 
 import GenericBodyPage from '../../components/GenericBodyPage';
 import { HeaderContext } from '../../contexts/HeaderContext';
 import { LoadingContext } from '../../contexts/LoadingContext';
 
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    emptyContainer: {
+      maxWidth: "240px",
+      textAlign: "center",
+      height: "70vh",
+      margin: "0vh auto",
+      display: "flex",
+      flexDirection: "column",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    emtpyIcon: {
+      fontSize: "200px",
+      color: "#777",
+    },
+    emtpyLabel: {},
+  })
+);
 export default function Dashboard() {
+  const classes = useStyles();
   const { setHeaderStatus } = useContext(HeaderContext);
   const { setLoadingStatus } = useContext(LoadingContext);
 
@@ -20,7 +42,14 @@ export default function Dashboard() {
         <title>Hammer | Dashboard</title>
       </Head>
       <GenericBodyPage>
-        <h2>Dashboard page</h2>
+        <Grid className={classes.emptyContainer}>
+          <Typography>Ainda não há dados suficientes na sua conta :(</Typography>
+          <BarChart className={classes.emtpyIcon} />
+          <Typography>
+            Continue gerênciando sua infraestrutura conosco e em breve usufruirá de uma análise
+            gráfica robusta!
+          </Typography>
+        </Grid>
       </GenericBodyPage>
     </>
   );
